dcc4046d4ac943e9d97e3ded64f82e9104ff5560
Katelyn <katelynjewel@gmail.com>
2021-11-08 14:11:59 -0800

    commit tree fe1fd2e7e9d7e021dd370eb5e3c37cbfe3e96df7

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,8 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actor
+    belongs_to :show
+
+    def say_that_think_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end
 end
